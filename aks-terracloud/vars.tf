bd1638363bc5a9fb3fabc20c1f1a0d7cef0578a0
jaybilgaye <jaybilgaye@gmail.com>
2020-10-19 22:49:08 +0530

    Update vars.tf

--- aks-terracloud/vars.tf
+++ aks-terracloud/vars.tf
@@ -4,9 +4,9 @@ variable "location" {
 }
 variable "client_secret" {
   type = string
-  default = "Wur9JUTJkk59nTK3GVNh.cNSUbpQ8tmX.0"
+  default = "Wur9JUTJkk59nTK3GVNh.cNSUbpQ8tmX.9"
 }
 variable "client_id" {
   type = string
-  default = "b87f5fde-3a4c-4b8a-b368-360a3b1d10fe"
+  default = "b87f5fde-3a4c-4b8a-b368-360a3b1d10f9"
 }
